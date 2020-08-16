e8973841cf8250c0b32e5a35a0e5c9d8ddf6aecc
John Stassen <john.stassen@gmail.com>
2020-08-16 18:00:48 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,8 +9,8 @@ def turn_count(board)
 end
 
 def current_player(board)
-  counter = turn_count(board)
-  if counter % 2 = 0
+  counter == turn_count(board)
+  if counter % 2 == 0
     return "X"
   else
     return "O"
